8d246667da5d535a150a4813a0b690b130c2eb54
Ludovic Andrieu <ludovic.andrieu@etu.u-bordeaux.fr>
2026-03-04 23:19:13 +0100

    chunck (+ tests) improvements

--- metrics/chunck.py
+++ metrics/chunck.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 from tokenizer_pfe.tokenizer_project import Tokenizer
 from collections import defaultdict
-from typing import Dict, Tuple
 import random
 
 """a partir des `logits` en entrée, fait un sampling dessus et les assemble pour renvoyer les differents svg qu'ils composent\n
@@ -22,13 +21,13 @@ import random
 
 """
 
-def sampling_logits(logits:torch.Tensor, tokenizer:Tokenizer, temperature=1.0, top_k=None, seed=-1):
+
+def sampling_logits(logits: torch.Tensor, temperature=1.0, top_k=None, seed=-1):
     """
     On passe le logit dans lequel on applique un sampling afin de donner une liste de tokens
 
     Entrée:
         logits: dict {(svgIndex, chunkIndex): torch.Tensor}
-        tokenizer: Tokenizer
         temperature: int
         top_k: int
         seed: int
@@ -39,12 +38,12 @@ def sampling_logits(logits:torch.Tensor, tokenizer:Tokenizer, temperature=1.0, t
     if seed >= 0:
         torch.manual_seed(seed)
         random.seed(seed)
-    
-    logits = logits.squeeze(0)
-    if top_k is not None and top_k > 0:
-        v, _ = torch.topk(logits, min(top_k, logits.size(-1)), dim=-1)
+    logits = logits.squeeze(0)  # (ctx_size, vocab_size)
+    ctx_size, vocab_size = logits.shape
+    if (top_k is not None) and (top_k > 0):
+        v, _ = torch.topk(logits, min(top_k, vocab_size),
+                          dim=-1)  # (ctx, topK)
         logits[logits < v[:, [-1]]] = -float('Inf')
-    
     if temperature > 0:
         logits = logits / temperature
         probs = F.softmax(logits, dim=-1)
@@ -54,10 +53,13 @@ def sampling_logits(logits:torch.Tensor, tokenizer:Tokenizer, temperature=1.0, t
     return tokens.tolist()
 
 
-def assemble_decode(logits: Dict[Tuple[int, int], torch.Tensor], tokenizer:Tokenizer, temperature=1.0, top_k=None, seed=-1)-> Dict[int, str]:
+def assemble_decode(
+        logits: dict[tuple[int, int], torch.Tensor],
+        tokenizer: Tokenizer, temperature=1.0, 
+        top_k=None, seed=-1) -> tuple[dict[int, str], dict[int, list[int]]]:
     """
     Assemble les chunks de tokens de chaque SVG puis les decodes.
-    
+
     Entrée:
         logits: dict {(svgIndex, chunkIndex): torch.Tensor}
         tokenizer: Tokenizer
@@ -66,17 +68,18 @@ def assemble_decode(logits: Dict[Tuple[int, int], torch.Tensor], tokenizer:Token
         seed: int
 
     Sortie:
-        svgs: Dictionnaire de taillle {int, str}
+        svgs: {svgID -> text decodé}
+        svgs_tokens: {svgID -> tokens samplés}
     """
-    svgs_tokens = {}
-    svgs = {}
+    svgs_tokens: dict[int, list[int]] = {}
+    svgs: dict[int, str] = {}
     for (svgIndex, chunckIndex), chunk_logits in sorted(logits.items(), key=None):
-        tokens = sampling_logits(chunk_logits, tokenizer, temperature, top_k, seed)
+        tokens = sampling_logits(chunk_logits, temperature, top_k, seed)
         if svgIndex not in svgs_tokens:
             svgs_tokens[svgIndex] = []
         svgs_tokens[svgIndex].extend(tokens)
-    
+
     for svg_idx, tokens in svgs_tokens.items():
         svgs[svg_idx] = tokenizer.decode(tokens)
-    
-    return svgs
+
+    return svgs, svgs_tokens
