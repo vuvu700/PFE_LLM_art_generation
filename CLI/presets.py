c417ed0ce76bbb7056a91eb41eb971eae7283001
Ludovic Andrieu <ludovic.andrieu@etu.u-bordeaux.fr>
2026-03-24 16:18:45 +0100

    memoire requis pour train

--- CLI/presets.py
+++ CLI/presets.py
@@ -5,7 +5,7 @@ PRESETS = {
     "1.6M": dict(depth=6, head_dim=128, context_size=4096, nb_heads_mult=1),
     "1.6M_ext": dict(depth=6, head_dim=128, context_size=4096 * 4, nb_heads_mult=1),
     "5.5M": dict(depth=6, head_dim=256, context_size=4096, nb_heads_mult=1),
-    "5.5M_ext": dict(depth=6, head_dim=256, context_size=4096 * 4, nb_heads_mult=1),
+    "5.5M_ext": dict(depth=6, head_dim=256, context_size=4096 * 4, nb_heads_mult=1),  # to train: 10Go
     "11.8M": dict(depth=6, head_dim=384, context_size=4096, nb_heads_mult=1),
     "20.5M": dict(depth=6, head_dim=512, context_size=4096, nb_heads_mult=5),
 }
